ae4ef204e16dc4ec5309de82dba26dd09e13119c
icatw <99238504+icatw@users.noreply.github.com>
2026-06-24 22:59:14 +0800

    feat(frontend): localize session reading controls

--- frontend/src/lib/components/content/CodeBlock.svelte
+++ frontend/src/lib/components/content/CodeBlock.svelte
@@ -4,6 +4,7 @@
   import { applyHighlight, applyMarks, clearMarks, escapeHTML } from "../../utils/highlight.js";
   import { highlightToHtml } from "../../utils/syntax-highlight.js";
   import CopyButton from "../shared/CopyButton.svelte";
+  import { m, t } from "../../i18n/index.js";
 
   interface Props {
     content: string;
@@ -70,10 +71,10 @@
   <CopyButton
     class="code-copy"
     {copied}
-    ariaLabel="Copy code block"
-    copiedAriaLabel="Copied code block"
-    title="Copy code"
-    copiedTitle="Copied!"
+    ariaLabel={t(m.code_block_copy_code_block)}
+    copiedAriaLabel={t(m.code_block_copied_code_block)}
+    title={t(m.code_block_copy_code)}
+    copiedTitle={t(m.code_block_copied)}
     onclick={handleCopy}
   />
   {#if language}
