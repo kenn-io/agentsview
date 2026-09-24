4f5d7be519b7be61466d33731d32d4ee020217b5
salmonumbrella <182032677+salmonumbrella@users.noreply.github.com>
2026-09-24 03:35:06 +0000

    test(postgres): include review tables in schema probes

--- internal/postgres/schema_test.go
+++ internal/postgres/schema_test.go
@@ -598,6 +598,9 @@ func TestSyncEnsureSchemaSkipsLegacyDDLWhenSchemaCompatible(t *testing.T) {
 		"cursor_usage_events":                             true,
 		"friction_findings":                               true,
 		"friction_session_dims":                           true,
+		"friction_digests":                                true,
+		"friction_digest_sessions":                        true,
+		"friction_patterns":                               true,
 	}
 	state.existingIndexes = map[string]bool{
 		"idx_cursor_usage_events_dedup":   true,
@@ -640,6 +643,9 @@ func TestEnsureSchemaScrubsProjectIdentityGitRemoteCredentials(t *testing.T) {
 		"cursor_usage_events":                             true,
 		"friction_findings":                               true,
 		"friction_session_dims":                           true,
+		"friction_digests":                                true,
+		"friction_digest_sessions":                        true,
+		"friction_patterns":                               true,
 	}
 	state.existingIndexes = map[string]bool{
 		"idx_cursor_usage_events_dedup":   true,
